443e1fe40197d3b77a43c161b8107e89b195e11b
Andres <andres.rivas@mercadolibre.com>
2020-06-05 15:20:25 -0300

    configure library with deeplink

--- app/src/main/java/com/ae/baseapplication/MainActivity.kt
+++ app/src/main/java/com/ae/baseapplication/MainActivity.kt
@@ -2,7 +2,10 @@ package com.ae.baseapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
+import com.ae.baseapplication.common.deepLink.DeepLinkFactory
+import com.ae.baseapplication.common.deepLink.DeepLinkFactoryImpl
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +16,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         container = findViewById(R.id.container)
+        setModules()
+    }
 
+    private fun setModules() {
+        addDeepLinkButton(DeepLinkFactoryImpl("mercadopago://send_money", "envio de dinero"))
+    }
 
+    private fun addDeepLinkButton(deepLinkFactory: DeepLinkFactory) {
+        val button = Button(this).apply {
+            text = deepLinkFactory.getTextToLabel()
+            buttonListener(this, deepLinkFactory)
+        }
+        container.addView(button)
+
+    }
+
+    private fun buttonListener(button: Button, deepLinkFactory: DeepLinkFactory) {
+        button.setOnClickListener {
+            startActivity(deepLinkFactory.createDeepLinkIntent(this))
+        }
     }
 }
---
+++ mylibrary/src/main/java/com/ae/mylibrary/LibraryActivity.kt
@@ -0,0 +1,12 @@
+package com.ae.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
